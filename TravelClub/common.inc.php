bc64630c3a469767192270a01952b9032e1c10b4
pandamc <juneblogs@gmail.com>
2017-03-21 16:21:22 +0000

    Add files via upload
    change path to css if page in members area 
    
    add function to check login and log page visit or redirect if user isn't logged in

--- TravelClub/common.inc.php
+++ TravelClub/common.inc.php
@@ -1,16 +1,26 @@
 <?php
 
+// require common files used by the application
+
+require_once( "config.php" );
+require_once( "Member.class.php" );
+require_once( "LogEntry.class.php" );
 // boiler plate for html pages to avoid code repetition
 // display header
-function displayPageHeader( $pageTitle ) {
+function displayPageHeader( $pageTitle, $membersArea = false ) {
     ?>
     <!DOCTYPE html>
     <html>
     <head>
         <meta charset="utf-8">
-        <link href="../common.css" rel="stylesheet" type="text/css">
+        <link href="<?php if ( $membersArea ) echo "../" ?>../common.css" rel="stylesheet" type="text/css">
         <title>Dynamic Web Applications</title>
         <style type="text/css">
+            .error {
+                background: #dd3333;
+                color: white;
+                padding: 0.2em;
+            }
             th {
                 text-align: left;
                 background-color: #bbbbbb;
@@ -34,5 +44,42 @@ function displayPageFooter() {
     </body>
     </html>
     <?php
+}
+
+// if field(s) not filled out change class and background colour
+function validateField( $fieldName, $missingFields ) {
+    if ( in_array( $fieldName, $missingFields ) ) {
+        echo ' class="error"';
+    }
+}
+
+// check value appropriate
+function setChecked( DataObject $obj, $fieldName, $fieldValue ) {
+    if ( $obj->getValue( $fieldName ) == $fieldValue ) {
+        echo ' checked="checked"';
+    }
+}
+
+// select value if appropriate
+function setSelected( DataObject $obj, $fieldName, $fieldValue ) {
+    if ( $obj->getValue( $fieldName ) == $fieldValue ) {
+        echo ' selected="selected"';
+    }
+}
+
+// check to see if user is logged in, if not direct to login page, otherwise log page visit
+function checkLogin() {
+    session_start();
+    if ( !$_SESSION["member"] or !$_SESSION["member"] = Member::getMember( $_SESSION["member"]->getValue( "id" ) ) ) {
+        $_SESSION["member"] = "";
+        header( "Location: login.php" );
+        exit;
+    } else {
+        $logEntry = new LogEntry( array (
+            "memberId" => $_SESSION["member"]->getValue( "id" ),
+            "pageUrl" => basename( $_SERVER["PHP_SELF"] )
+        ) );
+        $logEntry->record();
+    }
 }
 ?>
